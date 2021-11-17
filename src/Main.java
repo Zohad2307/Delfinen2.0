9e4a87e8de84956429fbd6874fc63b3c1b295d32
Zohad2307 <adel1086@stud.kea.dk>
2021-11-17 10:50:10 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-public static void main(String[] args){
 
-}
+
+
+    public static void main(String[] args){
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        Controller controller = new Controller();
+        UserInterface ui = new UserInterface(controller);
+        //controller.start();    Som kører program og loader filerne ind
+        ui.run();
+    }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,44 @@
 public class UserInterface {
+    private Controller controller;
+
+    UserInterface(Controller controller) {
+        this.controller = controller;
+
+    }
+
+    public void run(){
+        Menu menu = new Menu("Velkommen til Delfinen",
+                "Vælg en af ovennævnte muligheder", new String[]{"1. Opret medlem",
+                "2. Se topsvømmere", "3. Registrer resultat", "4. Se Regnskab", "5. Se menuoversigt", "9. Luk program"});
+
+        boolean isRunning = true;
+
+        menu.printMenu();
+        while(isRunning) {
+            switch(menu.readChoice()) {
+                case 1:
+                    //controller.createMember();
+                    break;
+                case 2:
+                    //controller.getTopFive();
+                    break;
+                case 3:
+                    //controller.registerResult();
+                    break;
+                case 4:
+                    //controller.getRegnskab();
+                    break;
+                case 5:
+                    menu.printMenu();
+                    break;
+                case 9:
+                    isRunning = false;
+                    //Eventuelt gemme det man på nuværende tidspunkt har lavet
+                    break;
+                default:
+                    System.out.println("Dit valg er ikke gyldigt.");
+
+            }
+        }
+    }
 }
